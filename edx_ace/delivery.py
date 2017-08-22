570a2e7db8d48c8e3a219f6398ced04c2a79d971
Gabe Mulley <gabe@edx.org>
2017-08-22 08:14:39 -0400

    handle recoverable errors

--- edx_ace/delivery.py
+++ edx_ace/delivery.py
@@ -1,6 +1,16 @@
+import datetime
+import logging
+import time
+
+from django.conf import settings
 import six
 
 from edx_ace.channel import channels
+from edx_ace.errors import RecoverableChannelDeliveryError
+from edx_ace.utils.date import get_current_time
+
+
+LOG = logging.getLogger(__name__)
 
 
 def deliver(channel_type, rendered_message, message):
@@ -10,7 +20,29 @@ def deliver(channel_type, rendered_message, message):
         raise ValueError(
             'No implementation for channel {channel_type} registered. Available channels are: {channels}'.format(
                 channel_type=channel_type,
-                channels=', '.join(six.text_type(channel) for channel in self.channels.keys())
+                channels=', '.join(six.text_type(channel) for channel in channels().keys())
             )
         )
-    return channel.deliver(message, rendered_message)
+    logger = message.get_message_specific_logger(LOG)
+
+    timeout_seconds = getattr(settings, 'ACE_DEFAULT_EXPIRATION_DELAY', 120)
+    default_expiration_time = get_current_time() + datetime.timedelta(seconds=timeout_seconds)
+    expiration_time = message.expiration_time or default_expiration_time
+    while get_current_time() < expiration_time:
+        logger.debug('Attempting delivery of message')
+        try:
+            return channel.deliver(message, rendered_message)
+        except RecoverableChannelDeliveryError as delivery_error:
+            num_seconds = (delivery_error.next_attempt_time - get_current_time()).total_seconds()
+            if delivery_error.next_attempt_time > expiration_time:
+                logger.debug('Message will expire before delivery can be reattempted, aborting.')
+                break
+            elif num_seconds > 0:
+                logger.debug(
+                    'Encountered a recoverable delivery error, retrying in %d seconds.',
+                    num_seconds
+                )
+                time.sleep(num_seconds)
+
+    logger.debug('Message expired before it could be successfully delivered.')
+    return False
--- edx_ace/errors.py
+++ edx_ace/errors.py
@@ -3,14 +3,12 @@
 #   channel subdirectory.
 
 class RecoverableChannelDeliveryError(Exception):
-    # TODO(now): Where is this handled?
     def __init__(self, message, next_attempt_time):
         self.next_attempt_time = next_attempt_time
         super(RecoverableChannelDeliveryError, self).__init__(message)
 
 
 class FatalChannelDeliveryError(Exception):
-    # TODO(now): Where is this handled?
     pass
 
 
