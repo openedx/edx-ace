8a4b5096fb1cb554f1b6cb0e10297db869960bfe
Gabe Mulley <gabe@edx.org>
2017-09-07 11:46:50 -0400

    update version to 0.1.2

--- edx_ace/__init__.py
+++ edx_ace/__init__.py
@@ -4,6 +4,6 @@ Framework for Messaging.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 default_app_config = 'edx_ace.apps.EdxAceConfig'
