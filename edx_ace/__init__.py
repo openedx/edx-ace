8114605fe7a5e64599d46ad081c5c130cae9ab53
Nimisha Asthagiri <nasthagiri@edx.org>
2017-09-16 13:46:37 -0400

    Update version to 0.1.4.

--- edx_ace/__init__.py
+++ edx_ace/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-__version__ = u'0.1.3'
+__version__ = u'0.1.4'
 
 default_app_config = u'edx_ace.apps.EdxAceConfig'
