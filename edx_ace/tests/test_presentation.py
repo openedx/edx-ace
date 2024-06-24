ba5de953b85d816b763651b68010eb5380a13c86
Іван Нєдєльніцев <ivan.niedielnitsev@raccoongang.com>
2024-06-24 09:51:28 +0300

    fix: [FC-0047] remove extra import

--- edx_ace/tests/test_presentation.py
+++ edx_ace/tests/test_presentation.py
@@ -4,7 +4,6 @@ Tests of :mod:`edx_ace.presentation`.
 from unittest import TestCase
 from unittest.mock import Mock
 
-from edx_ace.channel import ChannelType
 from edx_ace.errors import UnsupportedChannelError
 from edx_ace.presentation import render
 
