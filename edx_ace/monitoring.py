f73e45039b56f4ffb53900383f614f73b24543fb
muhammadadeeltajamul <muhammadadeeltajamul@hotmail.com>
2024-06-27 17:25:54 +0500

    chore: switch to edx monitoring for monitoring

--- edx_ace/monitoring.py
+++ edx_ace/monitoring.py
@@ -2,17 +2,8 @@
 :mod:`edx_ace.monitoring` exposes functions that are useful for reporting ACE
 message delivery stats to monitoring services.
 """
-try:
-    import ddtrace.auto
-except ImportError:
-    ddtrace = None  # pylint: disable=invalid-name
-from edx_django_utils.monitoring import DatadogBackend
+from edx_django_utils.monitoring import set_custom_attribute
 
 
 def report(key, value):
-    report_to_datadog(key, value)
-
-
-def report_to_datadog(key, value):
-    if ddtrace:
-        DatadogBackend().set_attribute(key, value)
+    set_custom_attribute(key, value)
