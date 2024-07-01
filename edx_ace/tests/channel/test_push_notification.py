557d47d16805f3ac19a45225022165d25ddcfb63
Іван Нєдєльніцев <ivan.niedielnitsev@raccoongang.com>
2024-07-01 13:01:30 +0300

    style: [FC-0047] fix import ordering

--- edx_ace/tests/channel/test_push_notification.py
+++ edx_ace/tests/channel/test_push_notification.py
@@ -1,10 +1,10 @@
 """
-
+Tests for the PushNotificationChannel class.
 """
-import pytest
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
+import pytest
 from firebase_admin.messaging import APNSConfig
 from push_notifications.models import GCMDevice
 
