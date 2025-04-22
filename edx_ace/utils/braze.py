b127a9fbc8dfcb5514b5ed6f6a0da6ba46a5bd71
Jawad Khan <jawadkhan444@gmail.com>
2025-04-22 12:11:34 +0500

    fix: regenerated requirements files

--- edx_ace/utils/braze.py
+++ edx_ace/utils/braze.py
@@ -14,14 +14,14 @@ def get_braze_client():
     if not BrazeClient:
         return None
 
-    braze_api_key = getattr(settings, 'EDX_BRAZE_API_KEY', None)
-    braze_api_url = getattr(settings, 'EDX_BRAZE_API_SERVER', None)
+    braze_api_key = getattr(settings, 'ACE_CHANNEL_BRAZE_API_KEY', None)
+    braze_api_url = getattr(settings, 'ACE_CHANNEL_BRAZE_REST_ENDPOINT', None)
 
     if not braze_api_key or not braze_api_url:
         return None
 
     return BrazeClient(
         api_key=braze_api_key,
-        api_url=braze_api_url,
+        api_url=f"https://{braze_api_url}",
         app_id='',
     )
