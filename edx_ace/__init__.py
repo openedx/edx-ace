deb0997ebfb5430e2f86b2855d419b37450f6cc8
jawad khan <jawadkhan444@gmail.com>
2025-04-22 16:24:21 +0500

    fix: Updated edx braze client version to latest (#350)

--- edx_ace/__init__.py
+++ edx_ace/__init__.py
@@ -13,7 +13,7 @@ from .policy import Policy, PolicyResult
 from .recipient import Recipient
 from .recipient_resolver import RecipientResolver
 
-__version__ = '1.12.0'
+__version__ = '1.12.1'
 
 
 __all__ = [
